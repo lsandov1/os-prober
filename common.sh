1ddc189210593c8daa49aa46349c0337dd8479a7
rpm-build <rpm-build>
2024-06-28 13:18:42 -0600

    os-prober-umount-fix.patch

--- common.sh
+++ common.sh
@@ -324,3 +324,13 @@ linux_mount_boot () {
 
 	mountboot="$bootpart $mounted"
 }
+
+umount_exec=$(which umount)
+umount() {
+	if ! $umount_exec $@ 2> /dev/null; then
+		error "umount error, retrying after 1 sec"
+		sleep 1
+		$umount_exec $@
+	fi
+}
+
